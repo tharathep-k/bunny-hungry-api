d47c7bcd533347027236e1f47ee8088f16d37996
tharathep-k <sun.tharathep@gmail.com>
2023-06-06 14:34:57 +0700

    finish:config middlewares

---
+++ src/app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan")
+const helmet = require("helmet")
+const rateLimit = require("express-rate-limit")
+
+const notFoundMiddleware = require("./middlewares/not-found");
+const errorMiddleware = require("./middlewares/error");
+
+const app = express();
+
+app.use(cors());
+
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"))
+}
+
+app.use(rateLimit({
+    windowMs: 1000 * 60 * 15,
+    max: 100,
+    message: {message: "Too many request"}
+
+}))
+
+app.use(helmet())
+app.use(express.json());
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
