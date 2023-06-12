2635f7378b7d2168233f5d5696a3975e92dc0a08
tharathep-k <sun.tharathep@gmail.com>
2023-06-12 21:01:10 +0700

    fix:login user+staff

--- src/controller/auth-controller.js
+++ src/controller/auth-controller.js
@@ -25,27 +25,44 @@ exports.register = async (req, res, next) => {
 
 exports.login = async (req, res, next) => {
   try {
-    const {mobile, password} =  validateLogin(req.body)
-    
-    const Staff = await staff.findOne({where: {mobile}})
-    const User = await user.findOne({where: {mobile}});
-    if (!User || !Staff) {
-      createError("invalid credential", 400);
+    const { mobile, password } = validateLogin(req.body);
+    // console.log(req.body);
+
+    const Staff = await staff.findOne({ where: { mobile } });
+    console.log(Staff);
+    const User = await user.findOne({ where: { mobile } });
+    if (!User && !Staff) {
+      createError("invalid credential1", 400);
     }
 
-    const isCorrect = await bcryptService.compare(
-      password,
-      User.password,
-    );
-    const isCorrectStaff = await bcryptService.compare(
-      password, Staff.password
-    )
-    if (!isCorrect || !isCorrectStaff) {
-      createError("invalid credential", 400);
+    if (User) {
+      const isCorrect = await bcryptService.compare(password, User.password);
+      if (!isCorrect) {
+        createError("invalid credential2", 400);
+      }
+      const accessToken = tokenService.sign({ id: User.id });
+      res.status(200).json({ accessToken });
     }
 
-    const accessToken = tokenService.sign({ id: User.id } || { id: Staff.id });
-    res.status(200).json({ accessToken });
+    if (Staff) {
+      const isCorrect = await bcryptService.compare(password, Staff.password);
+      if (!isCorrect) {
+        createError("invalid credential2", 400);
+      }
+      const accessToken = tokenService.sign({ id: Staff.id });
+      res.status(200).json({ accessToken });
+    }
+
+    // const isCorrectStaff = await bcryptService.compare(
+    //   password,
+    //   Staff.password
+    // );
+    // if (!isCorrect) {
+    //   createError("invalid credential2", 400);
+    // }
+
+    // const accessToken = tokenService.sign({ id: User.id });
+    // res.status(200).json({ accessToken });
   } catch (err) {
     next(err);
   }
