78f39f7c94f021faa454523807deeb9ab0604712
tharathep-k <sun.tharathep@gmail.com>
2023-06-14 20:47:55 +0700

    finish:clondinary

--- src/controller/product-controller.js
+++ src/controller/product-controller.js
@@ -1,16 +1,54 @@
 const { menu } = require("../models");
+const fs = require("fs");
+
+const uploadService = require("../services/upload-service");
+
+
+// exports.createMenu = async (req, res, next) => {
+//   console.log(req.body);
+//   try {
+//     const value = req.body
+//     value.staffId = req.Staff.id
+
+//     const Menu = await menu.create(value);
+//     console.log(Menu);
+//     res.status(200).json("Create menu success");
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
 exports.createMenu = async (req, res, next) => {
-  // console.log(req.body);
   try {
+    // image => req.file, message => req.body
+    if (!req.file && (!req.body.message || !req.body.message.trim())) {
+      createError("Message or image is required", 400);
+    }
+
     const value = req.body
-    value.staffId = req.Staff.id 
     
-    const Menu = await menu.create(value);
-    console.log(Menu);
-    res.status(200).json("Create menu success");
-  } catch (error) {
-    next(error);
+    console.log(value)
+    value.staffId = req.Staff.id
+
+    // const Menu = await menu.create(value);
+
+    if (req.body.message && req.body.message.trim()) {
+      value.message = req.body.message.trim();
+    }
+
+    if (req.file) {
+      const result = await uploadService.upload(req.file.path);
+      value.image = result.secure_url;
+    }
+
+    const menuPost = await menu.create(value);
+    res.status(201).json({ menuPost });
+  } catch (err) {
+    next(err);
+  } finally {
+    if (req.file) {
+      fs.unlinkSync(req.file.path);
+    }
   }
 };
 
@@ -30,17 +68,17 @@ exports.deleteMenu = async (req, res, next) => {
   try {
     const { menuid } = req.params;
     await menu.destroy({ where: { id: menuid } });
-    res.status(204).json("Delete menu success")
+    res.status(204).json("Delete menu success");
   } catch (error) {
     next(error);
   }
 };
 
-exports.getMenu = async (req, res ,next) => {
+exports.getMenu = async (req, res, next) => {
   try {
-    await menu.findAll({order: [["created_at", "DESC"]]})
-    res.status(200).json("Get menu success")
+    const getMenu = await menu.findAll({ order: [["created_at", "DESC"]] });
+    res.status(200).json(getMenu);
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -2,12 +2,14 @@ const express = require("express");
 
 const productController = require("../controller/product-controller");
 const authenticatedStaff = require("../middlewares/authenticated-staff");
+const upload = require("../middlewares/upload")
 
 const router = express.Router();
 
-router.post("/", authenticatedStaff, productController.createMenu)
+// router.post("/", authenticatedStaff, productController.createMenu)
 router.put("/:menuid", productController.updateMenu)
 router.delete("/:menuid", productController.deleteMenu)
 router.get("/menus", productController.getMenu)
+router.post("/", authenticatedStaff, upload.single("image") ,productController.createMenu)
 
 module.exports = router;
